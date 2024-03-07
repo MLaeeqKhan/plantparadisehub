a639e34a13f76428c1f7fafc0c57c2b9e7ba02ee
M. LAEEQ KHAN <110678008+MLaeeqKhan@users.noreply.github.com>
2024-03-07 08:50:29 +0500

    Update action.js

--- src/Redux/action.js
+++ src/Redux/action.js
@@ -1,6 +1,8 @@
 // actions.js
 export const SET_NOTIFICATION = 'SET_NOTIFICATION';
 export const SET_LAST_MESSAGE = 'SET_LAST_MESSAGE';
+export const SET_RECEIVER_ID = 'SET_RECEIVER_ID';
+
 
 
 export const setNotification = (newNotification) => ({
@@ -11,5 +13,10 @@ export const setNotification = (newNotification) => ({
 export const setLastMessage = (message) => ({
   type: SET_LAST_MESSAGE,
   payload: message
+});
+
+export const setReceiverId = (receiverId) => ({
+  type: SET_RECEIVER_ID,
+  payload: receiverId
 });
 
